58c91ca059924a9e36b0788f5c816286cb670526
AdeShaq <kvngshaggy@gmail.com>
2026-01-10 12:30:05 +0100

    update

--- components/RichEditor.tsx
+++ components/RichEditor.tsx
@@ -4,7 +4,7 @@ import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 import { Color } from '@tiptap/extension-color';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import TaskList from '@tiptap/extension-task-list';
 import TaskItem from '@tiptap/extension-task-item';
 import {
